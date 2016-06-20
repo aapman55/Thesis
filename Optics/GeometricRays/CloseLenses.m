144e60c9f506e83756708e7650cac68531743c87
aapman55 <zhi-li_liu@live.nl>
2016-06-20 16:55:00 +0200

    Updates: - Added direction for the lenses

---
+++ Optics/GeometricRays/CloseLenses.m
@@ -0,0 +1,19 @@
+% Close lenses test
+clear all; close all; clc;
+
+L1 = Lens(30, 0, 50);
+L2 = Lens(20,10, 50);
+
+O1 = LensObject(-50, 20);
+
+L1.setObject(O1);
+L1.computeImage();
+
+L2.setObject(L1.computedImage.toObject);
+L2.computeImage();
+
+figure;
+hold on;
+grid minor;
+L1.draw();
+L2.draw();
