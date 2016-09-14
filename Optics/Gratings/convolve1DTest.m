c1a8632546d9d1412e46c582e4dca1fcabafb582
aapman55 <zhi-li_liu@live.nl>
2016-09-14 14:50:03 +0200

    Changed: The way of generating the diffraction grating params

--- Optics/Gratings/convolve1DTest.m
+++ Optics/Gratings/convolve1DTest.m
@@ -1,14 +1,26 @@
 % This script tests the idea of convoluting a 1D shape into 1 line
 clear; close all; clc;
 %% Start program (input)
-% Amount of colors (amount of columns)
-amountOfColors = 100;
+% Amount of colors (amount of columns) [50]
+amountOfColors = 1000;
 
-% Amount of elements in the height direction (Amount of row entries)
-amountOfElementsInHeight = 100;
+% Amount of elements in the height direction (Amount of row entries) [100]
+amountOfElementsInHeight = amountOfColors;
 
-% Dictates the percentage of the center part blocked by the filter
-percentageBlocked = 0;
+% Dictates the percentage of the center part blocked by the filter [50]
+percentageBlocked = 50; 
+
+% Specify the portion you select from the generated spectrum (in
+% percentage) [45,70]
+beginSpectrum = 40;
+endSpectrum = 70;
+
+% Specify the speed of the diffraction grating (not in real /mm but just a
+% number) [3]
+speedGrating = 2;
+
+% Magnifying factor to put more brightness in the image; [2]
+brightnessFactor = 1;
 
 %% Initialisation
 % Get colors
@@ -23,7 +35,7 @@ base(:,:,2) = repmat(colors(2,:),amountOfElementsInHeight,1);
 base(:,:,3) = repmat(colors(3,:),amountOfElementsInHeight,1);
 
 % Put colors through slit to gather desired colors
-truncatedColors = base(:,45:70,:);
+truncatedColors = base(:,floor(beginSpectrum/100*amountOfColors):floor(endSpectrum*amountOfColors/100),:);
 
 %Show image
 image(truncatedColors)
@@ -40,20 +52,28 @@ truncatedColors(indexStart:indexStart+blockedElements-1,:,:) = blockingFilter;
 
 image(truncatedColors);
 
+newAmtColors = size(truncatedColors,2);
 % Create bigger zero matrix to be used for shifting the colors
-bigMatrix = repmat(zeros(size(truncatedColors)),2,1);
+bigMatrix = zeros(speedGrating*newAmtColors+amountOfElementsInHeight,newAmtColors,3);
 
 % Now for each column in the truncated Colors put them in the corresponding
 % row in the bigMatrix but shift every column 2 down with respect to the
 % previous column
 
 for i=0:size(truncatedColors, 2)-1
-    firstIndex = i*2+1;
+    firstIndex = i*speedGrating+1;
     lastIndex = firstIndex + size(truncatedColors, 1)-1;
     bigMatrix(firstIndex:lastIndex,i+1,:) = truncatedColors(:,i+1,:);
 end
 
-% squeeze them to one color
-squeezed = 5*sum(bigMatrix,2)/size(bigMatrix,2);
+figure()
+image(bigMatrix)
 
+minimumBrightness = min(min(min(bigMatrix)));
+maximumBrightness = max(max(max(bigMatrix)));
+factor = 1/(maximumBrightness-minimumBrightness)*3;
+
+% squeeze them to one color
+squeezed = brightnessFactor*factor*sum(bigMatrix,2)/size(bigMatrix,2);
+figure();
 image(squeezed);
